3919f5b8ea074453af1257a22a159297c243e5b8
timothy-spencer <timothy.spencer@gsa.gov>
2018-04-09 11:49:26 -0700

    fix pipeline and terraform scxripts

--- ci/terraform-secrets.sh
+++ ci/terraform-secrets.sh
@@ -3,8 +3,6 @@
 set -eux
 
 spruce merge --prune terraform_outputs \
-  postfix-config/varsfiles/${ENVIRONMENT}-terraform.yml \
   terraform-yaml/state.yml \
-  terraform-secrets/ns.yml \
   >> terraform-secrets/terraform.yml
 
