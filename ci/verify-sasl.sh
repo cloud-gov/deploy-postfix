cfde6f4dbd876e18564ae82b2307dff0817a2b3a
timothy-spencer <timothy.spencer@gsa.gov>
2018-04-09 15:57:06 -0700

    removed dkim check, changed secret example to not have redundant localhost

--- ci/verify-sasl.sh
+++ ci/verify-sasl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 set -eux
-# TODO
-return 1
+
+# Requires STARTTLS to be active.
+# Requires the cert to be trusted by the host that this is run on.
+echo -e "AUTH LOGIN\ntest\ntest\nQUIT" | openssl s_client -starttls smtp -crlf -quiet -connect 10.99.1.12:25 | grep ^334
