81897dedce0152fe10802e2427f904337387e63c
timothy-spencer <timothy.spencer@gsa.gov>
2018-04-10 10:22:19 -0700

    trying to use terraform IP for checks

--- ci/verify-sasl.sh
+++ ci/verify-sasl.sh
@@ -2,6 +2,8 @@
 
 set -eux
 
+SMTP_IP=$(spruce json terraform-yaml/state.yml | jq -r ".terraform_outputs.${ENVIRONMENT}_smtp_private_ip")
+
 # Requires STARTTLS to be active.
 # Requires the cert to be trusted by the host that this is run on.
-echo -e "AUTH LOGIN\ntest\ntest\nQUIT" | openssl s_client -starttls smtp -crlf -quiet -connect 10.99.1.12:25 | grep ^334
+echo -e "AUTH LOGIN\ntest\ntest\nQUIT" | openssl s_client -starttls smtp -crlf -quiet -connect "${SMTP_IP}:25" | grep ^334
