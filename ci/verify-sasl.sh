8dc15b71575ec51687545e5e2cf9128019623630
timothy-spencer <timothy.spencer@gsa.gov>
2018-04-11 11:45:10 -0700

    removed line as per josh, and fixed up comment to be proper in verify_sasl.sh

--- ci/verify-sasl.sh
+++ ci/verify-sasl.sh
@@ -5,5 +5,4 @@ set -eux
 SMTP_IP=$(spruce json terraform-yaml/state.yml | jq -r ".terraform_outputs.${ENVIRONMENT}_smtp_private_ip")
 
 # Requires STARTTLS to be active.
-# Requires the cert to be trusted by the host that this is run on.
 echo -e "AUTH LOGIN\ntest\ntest\nQUIT" | openssl s_client -starttls smtp -crlf -quiet -connect "${SMTP_IP}:25" | grep ^334
