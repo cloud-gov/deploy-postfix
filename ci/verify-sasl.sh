abcfbfc9f8a3d85e170e2d76e250595f1b94a579
Steven Harms <steven.harms@gsa.gov>
2018-06-07 11:30:05 -0400

    Update tests to validate username and passwords for success and failure

--- ci/verify-sasl.sh
+++ ci/verify-sasl.sh
@@ -4,5 +4,11 @@ set -eux
 
 SMTP_IP=$(spruce json terraform-yaml/state.yml | jq -r ".terraform_outputs.${ENVIRONMENT}_smtp_private_ip")
 
-# Requires STARTTLS to be active.
-echo -e "AUTH LOGIN\ntest\ntest\nQUIT" | openssl s_client -starttls smtp -crlf -quiet -connect "${SMTP_IP}:25" | grep ^334
+BASE64_USERNAME=$(echo -n "${USERNAME}" | base64)
+BASE64_PASSWORD=$(echo -n "${PASSWORD}" | base64)
+
+# Confirm bad username / password is denied
+echo -e "AUTH LOGIN\ntest\ntest\nQUIT" | openssl s_client -starttls smtp -crlf -quiet -connect "${SMTP_IP}:25" | grep ^535
+
+# Config good username / password is allowed
+echo -e "AUTH LOGIN\n${BASE64_USERNAME}\n${BASE64_PASSWORD}\nQUIT" | openssl s_client -starttls smtp -crlf -quiet -connect "${SMTP_IP}:25" | grep ^235
